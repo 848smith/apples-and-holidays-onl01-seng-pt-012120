26f6cd94fe0eef0f56daa8efa501b6a84bb87595
Andrew Smith <848smith@gmail.com>
2020-02-04 05:11:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,8 @@ def all_supplies_in_holidays(holiday_hash)
   end 
 end
 
-all_supplies_in_holidays(holiday_hash)
+def all_supplies_in_holidays(holiday_hash)
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
